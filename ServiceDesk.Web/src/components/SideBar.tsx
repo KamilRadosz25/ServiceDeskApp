a580d8ba6fc752837eab8d2c7a954808d1c9497f
KamilRadosz25 <kamil.radosz.25@gmail.com>
2023-04-04 23:03:50 +0200

    elo

--- ServiceDesk.Web/src/components/SideBar.tsx
+++ ServiceDesk.Web/src/components/SideBar.tsx
@@ -8,6 +8,7 @@ import { FiUsers } from "react-icons/fi";
 const SideBar = () => {
   return (
     <div
+      chuj
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark"
       style={{ width: "280px", height: "100vh" }}
     >
